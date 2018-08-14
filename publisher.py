60e2403f77303596dddfeaa8ffa6b3c18aee0325
Shriniwas Sharma <shriniwas26@gmail.com>
2018-08-14 14:57:15 +0200

    PEP8 compliant

--- publisher.py
+++ publisher.py
@@ -4,25 +4,29 @@ import paho.mqtt.client as mqtt
 import os, signal
 client = mqtt.Client("receiver")
 
+
 def on_publish(_client, _userdata, result):  # create function for callback
-    print("Data Published: " + str(result))
+    print("Message number: " + str(result))
+
 
 def on_disconnect(_client, _userdata, _result):
     print("Disconnected, killing app")
-    os.kill(os.getpid(), signal.SIGINT)
+    os.kill(os.getpid(), signal.SIGKILL)
+
 
 def on_connect(_client, _userdata, _flags, _result):
     print("Publisher connected")
 
-client = mqtt.Client("broadcaster")
-client.on_publish = on_publish
-client.on_disconnect = on_disconnect
-client.on_connect = on_connect
-assert client.connect(MQTT_BROKER_ADDR) == 0
-client.loop_start()
-i = 0
 
-while True:
-    i += 1
-    client.publish(MQTT_TOPIC, "Hello" + str(i), qos=1)
-    sleep(0.2)
+if __name__ == "__main__":
+    client = mqtt.Client("broadcaster")
+    client.on_publish = on_publish
+    client.on_disconnect = on_disconnect
+    client.on_connect = on_connect
+    assert client.connect(MQTT_BROKER_ADDR) == 0
+    client.loop_start()
+    i = 0
+    while True:
+        i += 1
+        client.publish(MQTT_TOPIC, "Hello", qos=1)
+        sleep(1)
--- receiver.py
+++ receiver.py
@@ -1,23 +1,29 @@
 from config import *
 import paho.mqtt.client as mqtt
+
 client = mqtt.Client("receiver")
+
 
 def on_connect(_client, _userdata, _flags, _rc):
     print("MQTT receiver connected!!")
     client.subscribe(MQTT_TOPIC)
 
+
 def on_disconnect(_client, _userdata, rc):
     if rc != 0:
         print("Unexpected disconnection of MQTT Broker")
-        quit()
+        os.kill(os.getpid(), signal.SIGKILL)
+
 
 def on_message(_client, _userdata, msg):
-    print("Topic: {}\nPayload: {}\n".format(msg.topic, msg.payload.decode("utf-8")))
-    data = msg.payload.decode("utf-8")
+    payload = msg.payload.decode("utf-8")
+    print("Topic: {}\nPayload: {}\n".format(msg.topic, payload))
 
-assert client.connect(MQTT_BROKER_ADDR) == 0
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.on_disconnect = on_disconnect
-client.loop_forever()
+if __name__ == "__main__":
+    assert client.connect(MQTT_BROKER_ADDR) == 0
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.on_disconnect = on_disconnect
+    client.loop_forever()
